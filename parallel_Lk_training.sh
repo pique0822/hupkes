c7c1c21d3ccfab4866321f727cc0187150053184
Miguel A Del Rio <drmiguel@openmind7.cm.cluster>
2019-08-13 17:33:55 -0400

    true parallel training

--- parallel_Lk_training.sh
+++ parallel_Lk_training.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+> training_ids.txt
+
 NUM_EPOCHS=1000
 for MODEL_ID in $(seq 1 20)
 do
 	echo Training Model ${MODEL_ID} for ${NUM_EPOCHS} epochs
 
-	sbatch --time 5 --job-name hupkes_${MODEL_ID} --gres=gpu:tesla-k80:1 Lk_training.sh ${MODEL_ID} ${NUM_EPOCHS} >> training_ids.txt
+	sbatch --time 1000 --job-name hupkes_${MODEL_ID} Lk_training.sh ${MODEL_ID} ${NUM_EPOCHS} >> training_ids.txt
 done
