abf6ca8d5f731b0acdcdbbe03b5f9e87499778e1
Miguel Del Rio <drmiguel@mit.edu>
2019-08-14 00:23:44 -0400

    changing the learning rate for all operations task

--- arithmetic_data/parallel_Lk_training.sh
+++ arithmetic_data/parallel_Lk_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NUM_EPOCHS=1000
+NUM_EPOCHS=3000
 DATASET=all_operations.txt
 BASE_NAME=all_operations
 VOCAB_FILE=datasets/all_operations_vocabulary.txt
@@ -13,5 +13,5 @@ for MODEL_ID in $(seq 1 20)
 do
 	echo Training Model ${BASE_NAME}_${MODEL_ID} for ${NUM_EPOCHS} epochs
 
-	sbatch --time 3000 --job-name ${BASE_NAME}_${MODEL_ID} Lk_training.sh ${BASE_NAME} ${MODEL_ID} ${NUM_EPOCHS} ${DATASET} ${VOCAB_FILE} ${LEARNING_RATE} >> ${BASE_NAME}_training_ids.txt
+	sbatch --time 5000 --job-name ${BASE_NAME}_${MODEL_ID} Lk_training.sh ${BASE_NAME} ${MODEL_ID} ${NUM_EPOCHS} ${DATASET} ${VOCAB_FILE} ${LEARNING_RATE} >> ${BASE_NAME}_training_ids.txt
 done
