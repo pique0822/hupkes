65269edb8de739ac37da9cee3a6daff133ca6af6
Miguel Del Rio <drmiguel@mit.edu>
2019-08-30 16:32:57 -0500

    adding transformer model

--- unique_sequence/parallel_transformer_training.sh
+++ unique_sequence/parallel_transformer_training.sh
@@ -17,7 +17,7 @@ do
 		do 
 			echo Training Model ${BASE_NAME}_${MODEL_ID} for ${NUM_EPOCHS} epochs
 
-			sbatch --time ${TIME} --job-name ${BASE_NAME}_${MODEL_ID} gru_training.sh ${BASE_NAME} ${MODEL_ID} ${NUM_EPOCHS} ${DATASET} ${VOCAB_FILE} ${NUM_LAYERS} ${NUM_HEADS} ${HIDDEN_SIZE} >> ${BASE_NAME}_training_ids.txt
+			sbatch --time ${TIME} --job-name ${BASE_NAME}_${MODEL_ID} transformer_training.sh ${BASE_NAME} ${MODEL_ID} ${NUM_EPOCHS} ${DATASET} ${VOCAB_FILE} ${NUM_LAYERS} ${NUM_HEADS} ${HIDDEN_SIZE} >> ${BASE_NAME}_training_ids.txt
 		done
 	done
 done
--- unique_sequence/test_transformer.py
+++ unique_sequence/test_transformer.py
@@ -12,17 +12,49 @@ from torch.autograd import Variable
 
 import numpy as np
 import os
+parser = argparse.ArgumentParser(description='Trains a GRU model on the arithmetic language dataset')
 
+# dataset information
+parser.add_argument('--dataset_file',type=str, default="ten_tokens_explicit_singular_data.txt",
+                    help='File that contains the full dataset from which we will take the training and test data.')
+parser.add_argument('--training_percent',type=float,
+                    default=0.6,
+                    help='Percent of data that is to be used as training.')
+parser.add_argument('--vocabulary_file',type=str,
+                    default='datasets/ten_tokens_explicit.txt',
+                    help='File containing each possible word in the training set.')
+parser.add_argument('--dataset_seed',type=int,
+                    default=None,
+                    help='Randomization seed for dataset.')
+parser.add_argument('--operation_type',type=str,
+                    default='singular',
+                    help='{singular | combined}')
+parser.add_argument('--transition_type',type=str,
+                    default='explicit',
+                    help='{implicit | explicit | repeated}')
+
+
+# model information
+parser.add_argument('--num_layers',type=int, default=2,
+                    help='The dimension of the model layers')
+parser.add_argument('--num_heads',type=int, default=2,
+                    help='The dimension of the model heads')
+parser.add_argument('--hidden_size',type=int, default=16,
+                    help='The dimension of the model hidden state')
+parser.add_argument('--model_save',type=str, default=None,
+                    help='File name to which we will save the model.')
+args = parser.parse_args()
+# hyper parameters
 vocabulary = []
-with open('datasets/ten_tokens_explicit.txt') as file:
+with open(args.vocabulary_file) as file:
     for line in file:
         vocabulary.append(line.strip())
 
 def data_sample(vocabulary, Ks=[2,4,5,7]):
     "Generate random data for a src-tgt copy task."
     pad = len(vocabulary)
-    output = generate_examples(transition_type = 'explicit', \
-                               operation_type = 'singular', \
+    output = generate_examples(transition_type = args.transition_type, \
+                               operation_type = args.operation_type, \
                                vocabulary = vocabulary, \
                                k = np.random.choice(Ks), \
                                num_examples=1).strip()
@@ -48,27 +80,32 @@ def data_sample(vocabulary, Ks=[2,4,5,7]):
 
 model = tr.make_transformer(src_vocab=len(vocabulary)+1, \
                         tgt_vocab=len(vocabulary)+1, \
-                        N=2, \
-                        d_model=16, d_ff=64, h=2, dropout=0.0)
-model.load_state_dict(torch.load('test.mdl'))
+                        N=args.num_layers, \
+                        d_model=args.hidden_size, \
+                        d_ff=4*args.hidden_size, \
+                        h=args.num_heads, dropout=0.0)
+model.load_state_dict(torch.load(args.model_save))
 model.eval()
 
 for k in range(2,11):
+	correct = 0
 	for ex in range(2000):
-		correct = 0
+		
 
 		src, tgt = data_sample(vocabulary, Ks=[k])
 		src = src.reshape(1,-1)
-		true = tgt[1]
+		true = tgt[1].item()
 
 		src_mask = (src != len(vocabulary)).unsqueeze(-2)
 		out = tr.greedy_decode(model, src, src_mask, max_len=2, start_symbol=len(vocabulary))
 
 		pred = out[0][1].item()
 
+		# print(pred, true)
 		if pred == true:
+			# print('CORR')
 			correct += 1
-
+	# import pdb; pdb.set_trace()
 	print('Dataset L'+str(k)+' Accuracy: '+str(round(correct/2000*100, 2)) + '%')
 
 
